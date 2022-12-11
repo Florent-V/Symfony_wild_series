389fcfa39de0e306bfe7eea5962cdad052a361a1
Florent-Vasseur <vasseurflorent@gmail.com>
2022-12-11 20:42:28 +0100

    quete 23 user creation

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+    public static int $userIndex = 0;
+
+    public const USER_INFOS = [
+        [
+            'email' => 'truc@mail.fr',
+            'pass' => 'azerty',
+            'username' => 'user1',
+            'role' => 'contributor'
+        ],
+        [
+            'email' => 'bidule@mail.fr',
+            'pass' => 'abcdef',
+            'username' => 'user2',
+            'role' => 'contributor'
+        ],
+        [
+            'email' => 'machin@mail.fr',
+            'pass' => 'supermdp',
+            'username' => 'user2',
+            'role' => 'admin',
+        ]
+    ];
+
+    private UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+
+        foreach (self::USER_INFOS as $userInfo) {
+            $user = new User();
+            $user->setEmail($userInfo['email']);
+
+            $hashedPassword = $this->passwordHasher->hashPassword(
+                $user,
+                $userInfo['pass']
+            );
+            $user->setPassword($hashedPassword);
+            $user->setRoles(array($userInfo['role']));
+            $user->setUsername($userInfo['username']);
+            $manager->persist($user);
+        }
+
+        $manager->flush();
+    }
+}
