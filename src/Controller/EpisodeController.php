06b9976306353d1cfbad051a716b6dac39ddcdbd
Florent-Vasseur <vasseurflorent@gmail.com>
2022-12-11 11:44:37 +0100

    quete_21 ok

--- src/Controller/EpisodeController.php
+++ src/Controller/EpisodeController.php
@@ -8,6 +8,8 @@ use App\Repository\EpisodeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\String\Slugger\SluggerInterface;
 
@@ -23,7 +25,10 @@ class EpisodeController extends AbstractController
     }
 
     #[Route('/new', name: 'new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EpisodeRepository $episodeRepository, SluggerInterface $slugger): Response
+    public function new(Request           $request,
+                        MailerInterface   $mailer,
+                        EpisodeRepository $episodeRepository,
+                        SluggerInterface  $slugger): Response
     {
         $episode = new Episode();
         $form = $this->createForm(EpisodeType::class, $episode);
@@ -33,6 +38,15 @@ class EpisodeController extends AbstractController
             $slug = $slugger->slug($episode->getTitle());
             $episode->setSlug($slug);
             $episodeRepository->save($episode, true);
+
+            //Sending Email
+            $email = (new Email())
+                ->from($this->getParameter('mailer_from'))
+                ->to('physchim115@gmail.com')
+                ->subject('Un nouvel épisode vient d\'être ajouté !')
+                ->html($this->renderView('episode/newEpisodeEmail.html.twig', ['episode' => $episode]));
+
+            $mailer->send($email);
 
             // Once the form is submitted, valid and the data inserted in database, creation success flash message
             $this->addFlash('success', 'The new episode has been created');
@@ -46,7 +60,8 @@ class EpisodeController extends AbstractController
         ]);
     }
 
-    #[Route('/{slug}', name: 'show', methods: ['GET'])]
+    #[
+        Route('/{slug}', name: 'show', methods: ['GET'])]
     public function show(Episode $episode): Response
     {
         return $this->render('episode/show.html.twig', [
@@ -75,7 +90,7 @@ class EpisodeController extends AbstractController
     #[Route('/{slug}', name: 'delete', methods: ['POST'])]
     public function delete(Request $request, Episode $episode, EpisodeRepository $episodeRepository): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$episode->getId(), $request->request->get('_token'))) {
+        if ($this->isCsrfTokenValid('delete' . $episode->getId(), $request->request->get('_token'))) {
             $episodeRepository->remove($episode, true);
             // creation warning flash message
             $this->addFlash('danger', 'The episode has been deleted');
--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -11,6 +11,9 @@ use App\Repository\ProgramRepository;
 use App\Service\ProgramDuration;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\String\Slugger\SluggerInterface;
@@ -32,7 +35,10 @@ class ProgramController extends AbstractController
     }
 
     #[Route('/new', name: 'new', methods: ['GET', 'POST'])]
-    public function new(Request $request, ProgramRepository $programRepository, SluggerInterface $slugger): Response
+    public function new(Request           $request,
+                        MailerInterface   $mailer,
+                        ProgramRepository $programRepository,
+                        SluggerInterface  $slugger): Response
     {
         $program = new Program();
 
@@ -43,18 +49,30 @@ class ProgramController extends AbstractController
         $form->handleRequest($request);
 
         // Was the form submitted ?
-        if ($form->isSubmitted()  && $form->isValid()) {
+        if ($form->isSubmitted() && $form->isValid()) {
             // Deal with the submitted data
             // For example : persiste & flush the entity
             // And redirect to a route that display the result
             $slug = $slugger->slug($program->getTitle());
             $program->setSlug($slug);
             $programRepository->save($program, true);
+
+            //Sending Email
+            $email = (new Email())
+                ->from($this->getParameter('mailer_from'))
+                ->to('physchim115@gmail.com')
+                ->subject('Une nouvelle série vient d\'être publiée !')
+                ->html($this->renderView('program/newProgramEmail.html.twig', ['program' => $program]));
+
+            $mailer->send($email);
+
+
             // Once the form is submitted, valid and the data inserted in database, creation success flash message
             $this->addFlash('success', 'The new program has been created');
+
             //Redirect to categories list
             return $this->redirectToRoute('program_index');
-            }
+        }
 
 
         // Render the form (best practice)
@@ -67,7 +85,7 @@ class ProgramController extends AbstractController
     #[Route('/{slug}', name: 'show', requirements: ['id' => '\d+'], methods: ['GET'])]
     public function show(Program $program, ProgramDuration $programDuration): Response
     {
-       
+
         if (!$program) {
             throw $this->createNotFoundException(
                 'No program with this id found in program\'s table.'
