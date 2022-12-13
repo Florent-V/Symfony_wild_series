8fe939fabd0a5838d00e8c3b01c6a6afaa98664a
Florent-Vasseur <vasseurflorent@gmail.com>
2022-12-13 17:40:47 +0100

    quete ok

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -45,6 +45,7 @@ class RegistrationController extends AbstractController
                 )
             );
 
+
             $entityManager->persist($user);
             $entityManager->flush();
 
@@ -74,6 +75,7 @@ class RegistrationController extends AbstractController
         // validate email confirmation link, sets User::isVerified=true and persists
         try {
             $this->emailVerifier->handleEmailConfirmation($request, $this->getUser());
+            $this->getUser()->setRoles(['ROLE_CONTRIBUTOR']);
         } catch (VerifyEmailExceptionInterface $exception) {
             $this->addFlash('verify_email_error', $translator->trans($exception->getReason(), [], 'VerifyEmailBundle'));
 
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use App\Entity\User;
+use Doctrine\DBAL\Types\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
@@ -18,6 +19,8 @@ class RegistrationFormType extends AbstractType
     {
         $builder
             ->add('email')
+            ->add('username')
+            ->add('bio')
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
