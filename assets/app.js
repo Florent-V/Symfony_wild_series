c820653e46f978812bc1700fb1fbf5b0b12180ee
Florent-V <vasseurflorent@gmail.com>
2024-01-04 21:49:18 +0100

    migration almost done

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,3 @@
-import './bootstrap.js';
 /*
  * Welcome to your app's main JavaScript file!
  *
@@ -7,9 +6,11 @@ import './bootstrap.js';
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/app.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@popperjs/core';
+//import './bootstrap.js';
 
 // start the Stimulus application
 // import './bootstrap';
